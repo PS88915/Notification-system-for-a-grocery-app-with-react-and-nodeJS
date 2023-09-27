5dc4adefa2929198d83de4785b8e26cfb50e6177
DESKTOP-3IUNHOJ\777 <xavi.biz.buzz@gmail.com>
2023-09-27 05:04:18 +0300

    initialization

---
+++ api/config.js
@@ -0,0 +1,12 @@
+
+const API_KEY = 'a6c29040c656e924b1ada839b0ad75d8';
+const SUBSCRIBER_ID = 'in-app-sandbox-subscriber-id-123';
+const EMAIL = 'xavi.biz.buzz@gmail.com';
+const PORT = 3000;
+
+export {
+    API_KEY,
+    EMAIL,
+    SUBSCRIBER_ID,
+    PORT
+}
